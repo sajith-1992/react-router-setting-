e6d89ebd8e32f7f3b900f7f10ad2e4c4aeffceb4
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2025-02-07 13:32:58 -0500

    link  to add page without reloading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import logo from './logo.svg';
 import About  from './container/About'
 import Profile from './container/Profile'
+import { BrowserRouter,Routes, Route,  Link } from "react-router-dom"
 
 import { useState } from 'react';
 
 function App() {
-let containner
-const [state,setstate]=useState()
-if(state === "about"){
-containner  = 
-<About></About>
-}
-if(state===  "profile"){
-  containner =     <Profile></Profile>
 
-}
   return (
 <div className="App">
 <button onClick={()=>setstate('about')}> About </button>
 <button onClick={()=>setstate('profile')}> profile</button>
 
+<BrowserRouter>
 
- {containner}
-</div>
-  );
+<Link to="/about">press</Link>
+      <Routes>
+        
+        <Route path="/about" Component={ About }>
+        </Route>
+      </Routes>
+  </BrowserRouter>
+
+
+</div>  );
 }
 
 export default App;
