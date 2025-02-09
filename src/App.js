63b187fed99c4b83f23636784521764ef14f946e
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2025-02-08 20:10:54 -0500

    button navigate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import logo from './logo.svg';
-import About  from './container/About'
-import Profile from './container/Profile'
-import { BrowserRouter,Routes, Route,  Link } from "react-router-dom"
-
+import About from './container/About';
+import Profile from './container/Profile';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 import { useState } from 'react';
 
 function App() {
-
+ const navigate = useNavigate() 
   return (
-<div className="App">
-<button onClick={()=>setstate('about')}> About </button>
-<button onClick={()=>setstate('profile')}> profile</button>
+  
+    
+      <div className="App">
+        <nav>
+          <button onClick={() => navigate('/about')}>About</button>
+          <button onClick={() => navigate('/profile')}>Profile</button>
+        </nav>
 
-<BrowserRouter>
-
-<Link to="/about">press</Link>
-<Link to="/profile">profilepress</Link>
-      <Routes>
-        
-        <Route path="/about" Component={ About } />
-        <Route path="/profile" element={<h1>hello world</h1>} />
-        
-      </Routes>
-  </BrowserRouter>
-
-
-</div>  );
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    
+  );
 }
 
 export default App;
